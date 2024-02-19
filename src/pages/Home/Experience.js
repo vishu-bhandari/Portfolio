900b6f7b09bf6890b7e7fd861dfa24eb055f211d
vishu bhandari <deevishu1234@gmail.com>
2024-02-19 12:58:59 +0530

    experiences created

--- src/pages/Home/Experience.js
+++ src/pages/Home/Experience.js
@@ -1,12 +1,44 @@
-import React from 'react'
-import SectionTitle from '../../components/SectionTitle'
+import React from "react";
+import SectionTitle from "../../components/SectionTitle";
+import { Experiences } from "../../resources/Experiences";
 
 function Experience() {
+  const [selectedItemIndex, setselectedItemIndex] = React.useState(0);
   return (
     <>
-        <SectionTitle title="Projects"/>
+      <SectionTitle title="Experience" />
+      <div className="flex py-10">
+        <div className="flex flex-col gap-10 border-l-2 border-[#135e4c82] ">
+         
+            {Experiences.map((experience, index) => (
+              <>
+                <div
+                  className="cursor-pointer "
+                  onClick={() => {
+                    setselectedItemIndex(index);
+                    console.log(index);
+                  }}
+                >
+                  <div
+                    className={`text-xl  px-10 py-2 ${
+                      selectedItemIndex === index
+                        ? "text-tertiary border-tertiary border-l-4 -ml-[3px] bg-tertiary bg-opacity-20 "
+                        : "text-white "
+                    }`}
+                  >
+                    {experience.period}
+                  </div>
+                </div>
+              </>
+            ))}
+         
+          <div>
+            
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
