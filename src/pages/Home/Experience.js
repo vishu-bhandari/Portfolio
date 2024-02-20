56e56ddcf793e16808f51021be829d856c762c2d
vishu bhandari <deevishu1234@gmail.com>
2024-02-20 15:39:47 +0530

    experience added

--- src/pages/Home/Experience.js
+++ src/pages/Home/Experience.js
@@ -7,8 +7,8 @@ function Experience() {
   return (
     <>
       <SectionTitle title="Experience" />
-      <div className="flex py-10">
-        <div className="flex flex-col gap-10 border-l-2 border-[#135e4c82] ">
+      <div className="flex py-10 gap-20 sm:flex-col flex-row ">
+        <div className="flex flex-col gap-10 border-l-2 border-[#135e4c82] w-1/3 sm:flex-row sm:overflow-x-scroll sm:w-full">
          
             {Experiences.map((experience, index) => (
               <>
@@ -22,7 +22,7 @@ function Experience() {
                   <div
                     className={`text-xl  px-10 py-2 ${
                       selectedItemIndex === index
-                        ? "text-tertiary border-tertiary border-l-4 -ml-[3px] bg-tertiary bg-opacity-20 "
+                        ? "text-tertiary border-tertiary border-l-4 -ml-[3px] bg-tertiary bg-opacity-20 sm:w-full"
                         : "text-white "
                     }`}
                   >
@@ -31,11 +31,21 @@ function Experience() {
                 </div>
               </>
             ))}
+            </div>
          
-          <div>
-            
+          <div className="gap-8 flex flex-col ">
+            <h1 className="text-secondary text-4xl flex">
+              {Experiences[selectedItemIndex].title}
+            </h1>
+            <h1 className="text-tertiary text-2xl flex">
+              {Experiences[selectedItemIndex].company}
+            </h1>
+            <h1 className="text-white text-lg flex">
+              {Experiences[selectedItemIndex].description}
+            </h1>
+          
           </div>
-        </div>
+        
       </div>
     </>
   );
